f428ed33cd3a7bc527da44e44d2c239ab2ff70c1
izidormaklary <maklaryizidor@gmail.com>
2021-04-29 16:26:22 +0200

    working rulez

--- script.js
+++ script.js
@@ -13,13 +13,14 @@
     let scorePlayer = 0;
     let scorePC = 0;
 
+    // each card goes from the deck to the players hand
     function givePlayerCard() {
-        playerHand.push(playingDeck.pop());
+        playerHand.push(playingDeck.pop());    // each card goes from the deck array to the players hand array
         if(playerHand[playerHand.length-1].value === "A" && scorePlayer + playerHand[playerHand.length-1].weight > 21 ){
             playerHand[playerHand.length-1].weight = 1;
-        }
-        scorePlayer = scorePlayer + playerHand[playerHand.length-1].weight;
-        let imgLink;
+        }                                                                       // if an ace with 11 weight, points would be more than 21 ==> weight is 1
+        scorePlayer = scorePlayer + playerHand[playerHand.length-1].weight;     // weight of card adds to the score
+        let imgLink;                                                            // select img source according to suits
         switch (playerHand[playerHand.length-1].suits){
             case 'H': imgLink="resources/h.png"
                 break;
@@ -30,16 +31,17 @@
             case 'S': imgLink="resources/s.png"
                 break;
         }
-        playerScore.innerHTML = scorePlayer;
+        playerScore.innerHTML = scorePlayer;                                // print the score in html
+        //print the card body in html as div
         pCards.innerHTML = pCards.innerHTML +'<div class="cards"> <h2>'+ playerHand[playerHand.length-1].value + '</h2> <img src="'+ imgLink + '" alt="' + playerHand[playerHand.length-1].suits + '" width="35" height="35"> </div>';
     }
     function giveComputerCard() {
-        pcHand.push(playingDeck.pop());
+        pcHand.push(playingDeck.pop());         // each card goes from the deck array to the players hand array
         if(pcHand[pcHand.length-1].value === "A" && scorePC + pcHand[pcHand.length-1].weight > 21 ){
             pcHand[pcHand.length-1].weight = 1;
-        }
-        scorePC = scorePC + pcHand[pcHand.length-1].weight;
-        let imgLink;
+        }                                                                     // if an ace with 11 weight, points would be more than 21 ==> weight is 1
+        scorePC = scorePC + pcHand[pcHand.length-1].weight;                   // weight of card adds to the score
+        let imgLink;                                                          // select img source according to suits
         switch (pcHand[pcHand.length-1].suits){
             case 'H': imgLink="resources/h.png"
                 break;
@@ -50,9 +52,11 @@
             case 'S': imgLink="resources/s.png"
                 break;
             }
-            pcScore.innerHTML= scorePC;
+            pcScore.innerHTML= scorePC;                                         // print the score in html
+            //print the card body in html as div
             pcCards.innerHTML = pcCards.innerHTML +'<div class="cards"> <h2>'+ pcHand[pcHand.length-1].value + '</h2> <img src="'+ imgLink + '" alt="' + pcHand[pcHand.length-1].suits + '" width="35" height="35"> </div>';
     }
+    //creating deck 4*13 cards with multiple classes
     function createDeck() {
         let suits = ['H', 'C', 'D', 'S'];
         let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
@@ -68,6 +72,7 @@
         }
         return deck;
     }
+    //random schuffle
     function schuffleDeck(deck) {
         for (let i = 0; i < 52; i++) {
             let tempCard = deck[i];
@@ -76,6 +81,8 @@
             deck[randindex] = tempCard;
         }
     }
+    // start game buttons function,
+    // prepares the deck, and gives each player 2 cards, then the start button disappears, and the 2 reaction buttons appear for the game
     function startGame() {
         scorePlayer = 0;
         scorePC = 0;
@@ -95,12 +102,6 @@
                 giveComputerCard();
             }
         }
-      //  buttons.innerHTML= '<td>\n' +
-      //      '                   <button type="button" id="newcard" >get card</button>\n' +
-      //      '               </td>\n' +
-      //      '               <td>\n' +
-      //      '                   <button type="button" id="stall">stall</button>\n' +
-      //      '               </td>';
         playingButtons.style.display= "table-row";
         buttons.style.display= "none";
 
@@ -123,27 +124,39 @@
         playingButtons.style.display= "none";
     }
     function blackJack(){
-        heading.innerHTML = "Blackjack for you";
+        heading.innerHTML = "Blackjack";
         buttons.style.display= "table-row";
         playingButtons.style.display= "none";
     }
 
 
 
-   // get card
+   // adds additional card to players hand, and to computers hand if its score is not more than 15, possible outcomes
    function getCard(){
        givePlayerCard();
        if (scorePC < 16) {
            giveComputerCard();
        }
-       if (scorePC === 21 || scorePlayer > 21){
-           youLost();
-       }else if (scorePlayer === 21 || scorePC > 21){
-           youWon();
+       if (scorePC === 21){
+           if (scorePlayer === scorePC){
+               Push();
+           }else{
+               blackJack();
+               setTimeout(youLost, 3000);
+           }
+
+       }else if (scorePlayer === 21) {
+          blackJack();
+          setTimeout(youWon, 3000);
+       }else if (scorePlayer > 21){
+           youLost()
+       } else if (scorePC > 21) {
+           youWon()
        }
+
     }
 
-
+    // player won't pull more cards, computer only if its score is lower then players
     function Stall(){
         if (scorePlayer > scorePC ) {
             giveComputerCard();
@@ -151,26 +164,33 @@
         }else{
             evaluate();
         }
+
     }
 
-
+     // checking for outcomes
     function evaluate(){
         if (scorePlayer === scorePC) {
             Push();
-
         }else if (scorePlayer > 21 ){
             youLost()
         } else if (scorePC > 21 ){
             youWon ();
         } else if (scorePC === 21 ){
-            youLost();
+            blackJack();
+            setTimeout(youLost, 3000);
         }else if (scorePlayer === 21){
+            blackJack();
+            setTimeout(youWon, 3000);
+
+        }else if (scorePC < scorePlayer ){
             youWon();
+        }else {
+            youLost()
         }
 
     }
 
-
+    // button eventlisteners
     const play = document.getElementById('start')
     const gimme = document.getElementById('newcard')
     const stall = document.getElementById('stall')
